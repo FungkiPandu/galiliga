c23da6ce6eebd401ab8d7645a95ab5286383e963
Fungki Pandu F <neopandu@gmail.com>
2019-11-03 17:28:10 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+const leagues = [
+    "English Premier League",
+    "German Bundesliga",
+    "Italian Serie A",
+    "French Ligue 1",
+    "Spanish La Liga"
+];
+
+let sidenav = $('.sidenav');
+
+$(document).ready(function () {
+    sidenav.sidenav();
+});
